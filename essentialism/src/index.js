c7d318bf499de4b6931a11309b3b52c235ee73dd
Dan Sherman <dvsherman@protonmail.com>
2019-03-11 14:10:42 -0700

    built out file/folder structure

--- essentialism/src/index.js
+++ essentialism/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { RootContext } from './RootContext';
+import { RootContext } from './RootContext';
 
 ReactDOM.render(
     // <RootContext>
