ad76b65552b62dd11c9edbfedd2327e3ddccccf5
Dan Sherman <dvsherman@protonmail.com>
2019-03-14 10:34:29 -0700

    added _redirects file

--- essentialism/src/components/Project.js
+++ essentialism/src/components/Project.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class Project extends Component {
   constructor(props) {
     super(props)
   }
+
+  deleteProject = id => {
+    this.props.deleteProject(id);
+  };
+
   render() {
     return (
       <div>
-        <p>{this.props.project}</p>
+        <div>
+          <p>{this.props.project} <span>x</span></p>
+        </div>
+        <div>
+
+        </div>
       </div>
     )
   }
 }
 
-export default Project;
+const mapStateToProps = ({ deleteProject }) => ({
+  deleteProject
+})
+export default connect(
+  mapStateToProps,
+  { deleteProject }
+)(Project);
